22fde9d5e23898104297aa017cb94e89d06a3d93
pguyon <pguyon69@gmail.com>
2022-05-10 15:18:21 +0200

    add pictures

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,18 @@
 //Mettre le code JavaScript lié à la page photographer.html
+/* async function currentPhotographer() {
+  let res = await fetch(`./data/photographers.json`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+    throw new Error(`une erreur c'est produite`);
+  }
+
+  let data = await res.json();
+  const photographers = data.photographers;
+  console.log(photographers);
+}
+
+currentPhotographer(); */
