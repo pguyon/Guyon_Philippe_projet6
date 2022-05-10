8dc87c991a1c2107f39ec9732d3a27aa5bc7f6cd
pguyon <pguyon69@gmail.com>
2022-05-10 08:34:58 +0200

    step 2 down, add properties to photographerFactory

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,26 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const span = document.createElement("span");
+    h2.textContent = name;
+    h3.textContent = `${city} , ${country}`;
+    p.textContent = tagline;
+    span.textContent = `${price}€/jour`;
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(p);
+    article.appendChild(span);
+    return article;
+  }
+  return { name, picture, city, country, tagline, price, getUserCardDOM };
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,19 +1,19 @@
 async function getPhotographers() {
-  let res = await fetch('/data/photographers.json', {
-    headers:{
+  let res = await fetch("/data/photographers.json", {
+    headers: {
       Accept: "application/json",
-      "Content-Type": "application/json"
-    }
-  })
-  
-  if(!res.ok){
-    throw new Error('Une erreur c\'est produite')
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Une erreur c'est produite");
   }
-  let data = await res.json()
-   const photographers = data.photographers;
-    displayData(photographers)   
-    console.log(photographers)
-  
+
+  let data = await res.json();
+  const photographers = data.photographers;
+  displayData(photographers);
+  console.log(photographers);
 }
 
 async function displayData(photographers) {
@@ -25,8 +25,4 @@ async function displayData(photographers) {
   });
 }
 
-
-
 getPhotographers();
-
-
