d57b3b8bb8b1e94065fac8fddf5b0eb36b56846c
pguyon <pguyon69@gmail.com>
2022-05-10 10:11:52 +0200

    add alt on img

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,26 +1,47 @@
-function photographerFactory(data) {
-  const { name, portrait, city, country, tagline, price, id } = data;
-  const picture = `assets/photographers/${portrait}`;
+// function photographerFactory(data) {
+//   const { name, portrait, city, country, tagline, price } = data;
+//   const picture = `assets/photographers/${portrait}`;
 
+//   function getUserCardDOM() {
+//     const article = document.createElement("article");
+//     const img = document.createElement("img");
+//     img.setAttribute("src", picture);
+//     img.alt = `Picture of ${name}`;
+//     const h2 = document.createElement("h2");
+//     const h3 = document.createElement("h3");
+//     const p = document.createElement("p");
+//     const span = document.createElement("span");
+//     h2.textContent = name;
+//     h3.textContent = `${city} , ${country}`;
+//     p.textContent = tagline;
+//     span.textContent = `${price}€/jour`;
+//     article.appendChild(img);
+//     article.appendChild(h2);
+//     article.appendChild(h3);
+//     article.appendChild(p);
+//     article.appendChild(span);
+//     return article;
+//   }
+//   return { name, picture, city, country, tagline, price, getUserCardDOM };
+// }
+
+function photographerFactory(data) {
+  const photographerSection = document.querySelector(".photographer_section");
+  const { name, portrait, city, country, tagline, price } = data;
+  const picture = `assets/photographers/${portrait}`;
   function getUserCardDOM() {
-    const article = document.createElement("article");
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    img.alt = `Picture of ${name}`;
-    const h2 = document.createElement("h2");
-    const h3 = document.createElement("h3");
-    const p = document.createElement("p");
-    const span = document.createElement("span");
-    h2.textContent = name;
-    h3.textContent = `${city} , ${country}`;
-    p.textContent = tagline;
-    span.textContent = `${price}€/jour`;
-    article.appendChild(img);
-    article.appendChild(h2);
-    article.appendChild(h3);
-    article.appendChild(p);
-    article.appendChild(span);
-    return article;
+    let article = `
+        <article>
+        <img src="${picture}" alt="Picure of ${name}"/>
+        <h2>${name}</h2>
+        <h3>${city} , ${country}</h3>
+        <p>${tagline}</p>
+        <span>${price}€/jour</span>
+        </article>
+      
+      `;
+    photographerSection.innerHTML = article;
+    return photographerSection;
   }
-  return { name, picture, city, country, tagline, price, getUserCardDOM };
+  return getUserCardDOM();
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -18,7 +18,7 @@ async function getPhotographers() {
 
 async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
-  photographers.forEach((photographer) => {
+  photographers.map((photographer) => {
     const photographerModel = photographerFactory(photographer);
     const userCardDOM = photographerModel.getUserCardDOM();
     photographersSection.appendChild(userCardDOM);
