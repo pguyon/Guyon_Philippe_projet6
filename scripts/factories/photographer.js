5e513005c7163f2b2b7fa96f21c106836028f040
pguyon <pguyon69@gmail.com>
2022-05-10 10:52:55 +0200

    update code

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,29 +1,29 @@
-// function photographerFactory(data) {
-//   const { name, portrait, city, country, tagline, price } = data;
-//   const picture = `assets/photographers/${portrait}`;
+/*  function photographerFactory(data) {
+   const { name, portrait, city, country, tagline, price } = data;
+   const picture = `assets/photographers/${portrait}`;
 
-//   function getUserCardDOM() {
-//     const article = document.createElement("article");
-//     const img = document.createElement("img");
-//     img.setAttribute("src", picture);
-//     img.alt = `Picture of ${name}`;
-//     const h2 = document.createElement("h2");
-//     const h3 = document.createElement("h3");
-//     const p = document.createElement("p");
-//     const span = document.createElement("span");
-//     h2.textContent = name;
-//     h3.textContent = `${city} , ${country}`;
-//     p.textContent = tagline;
-//     span.textContent = `${price}€/jour`;
-//     article.appendChild(img);
-//     article.appendChild(h2);
-//     article.appendChild(h3);
-//     article.appendChild(p);
-//     article.appendChild(span);
-//     return article;
-//   }
-//   return { name, picture, city, country, tagline, price, getUserCardDOM };
-// }
+   function getUserCardDOM() {
+     const article = document.createElement("article");
+     const img = document.createElement("img");
+     img.setAttribute("src", picture);
+     img.alt = `Picture of ${name}`;
+     const h2 = document.createElement("h2");
+     const h3 = document.createElement("h3");
+     const p = document.createElement("p");
+     const span = document.createElement("span");
+     h2.textContent = name;
+     h3.textContent = `${city} , ${country}`;
+     p.textContent = tagline;
+     span.textContent = `${price}€/jour`;
+     article.appendChild(img);
+     article.appendChild(h2);
+     article.appendChild(h3);
+     article.appendChild(p);
+     article.appendChild(span);
+     return article;
+   }
+   return { name, picture, city, country, tagline, price, getUserCardDOM };
+ } */
 
 function photographerFactory(data) {
   const photographerSection = document.querySelector(".photographer_section");
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -17,12 +17,14 @@ async function getPhotographers() {
 }
 
 async function displayData(photographers) {
-  //   const photographersSection = document.querySelector(".photographer_section");
-  photographers.map((photographer) => {
+/*    const photographersSection = document.querySelector(".photographer_section");
+ */ 
+ photographers.map((photographer) => {
     const photographerModel = photographerFactory(photographer);
      const userCardDOM = photographerModel.getUserCardDOM();
-    // photographersSection.appendChild(userCardDOM);
-    return userCardDOM;
+/*      photographersSection.appendChild(userCardDOM);
+ */   
+ return userCardDOM;
    
   });
 }
