90ac19ebb44810fd6124c383ebb077cf3e72ab37
pguyon <pguyon69@gmail.com>
2022-05-10 08:43:46 +0200

    add alt on img in photographerFacroty

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,12 +1,12 @@
 function photographerFactory(data) {
   const { name, portrait, city, country, tagline, price } = data;
-
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
     const img = document.createElement("img");
     img.setAttribute("src", picture);
+    img.alt = `Picture of ${name}`;
     const h2 = document.createElement("h2");
     const h3 = document.createElement("h3");
     const p = document.createElement("p");
