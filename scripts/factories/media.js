2500c94ca65df0362f4d2b9e68f5a9f289f2cfd2
pguyon <pguyon69@gmail.com>
2022-05-12 09:36:34 +0200

    add photograph media

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,10 +1,13 @@
 function mediaFactories(value) {
   const { photographerId, title, image, video, likes, date, price } = value;
-  const mediaType;
-  const image = 
+  let mediaType;
+  const images = `./assets/photos/${photographerId}/${image}`;
+  const videos = `./assets/photos/${photographerId}/${video}`;
 
   function getMediaCardDom() {
+    const main = document.querySelector(".main");
     const mediaDiv = document.createElement("div");
+    const imgMedia = document.createElement("img");
   }
 
   return {
