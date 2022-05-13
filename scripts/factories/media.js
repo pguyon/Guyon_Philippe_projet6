5a9b5aa01ea5bb9a08bc1368d6eefca65ef186cf
pguyon <pguyon69@gmail.com>
2022-05-13 12:58:33 +0200

    add video

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,19 +1,32 @@
 function mediaFactories(value) {
   const { photographerId, title, image, video, likes, date, price } = value;
-  let mediaType;
-  const images = `./assets/photos/${photographerId}/${image}`;
-  const videos = `./assets/photos/${photographerId}/${video}`;
+  let mediaType, media;
+
+  mediaType = image ? 'image' : 'video';
+
+  media = mediaType == 'image' ? `./assets/photos/${photographerId}/${image}`: `./assets/photos/${photographerId}/${video}`;
+  /* const images = `./assets/photos/${photographerId}/${image}`;
+  const videos = `./assets/photos/${photographerId}/${video}`; */
 
   function getMediaCardDom() {
     const article = document.createElement("article");
     const imgMedia = document.createElement("img");
+    const videoMedia = document.createElement('video')
     const h2 = document.createElement("h2");
 
-    imgMedia.setAttribute("src", images);
+    imgMedia.setAttribute("src", media);
     imgMedia.alt = `Picture of ${title}`;
+    videoMedia.setAttribute("src", media);
 
     h2.textContent = title;
-    article.appendChild(imgMedia);
+
+    if(mediaType == 'image'){
+      article.appendChild(imgMedia);
+    }else{
+      article.appendChild(videoMedia);
+
+    }
+   
     article.appendChild(h2);
 
     return article;
