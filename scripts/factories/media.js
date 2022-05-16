40121eb311f3ef5b4df98151c052bad9a25e20a2
pguyon <pguyon69@gmail.com>
2022-05-16 14:25:46 +0200

    update css

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -2,17 +2,18 @@ function mediaFactories(value) {
   const { photographerId, title, image, video, likes, date, price } = value;
   let mediaType, media;
 
-  mediaType = image ? 'image' : 'video';
+  mediaType = image ? "image" : "video";
 
-  media = mediaType == 'image' ? `./assets/photos/${photographerId}/${image}`: `./assets/photos/${photographerId}/${video}`;
-  /* const images = `./assets/photos/${photographerId}/${image}`;
-  const videos = `./assets/photos/${photographerId}/${video}`; */
+  media =
+    mediaType == "image"
+      ? `./assets/photos/${photographerId}/${image}`
+      : `./assets/photos/${photographerId}/${video}`;
 
   function getMediaCardDom() {
     const article = document.createElement("article");
     const imgMedia = document.createElement("img");
-    const videoMedia = document.createElement('video')
-    const source = document.createElement('source')
+    const videoMedia = document.createElement("video");
+    const source = document.createElement("source");
     const h2 = document.createElement("h2");
 
     imgMedia.setAttribute("src", media);
@@ -20,18 +21,17 @@ function mediaFactories(value) {
     source.setAttribute("src", media);
     videoMedia.controls = true;
     videoMedia.autoplay = false;
-    videoMedia.muted = false
+    videoMedia.muted = false;
 
     h2.textContent = title;
 
-    if(mediaType == 'image'){
+    if (mediaType == "image") {
       article.appendChild(imgMedia);
-    }else{
-      videoMedia.appendChild(source)
+    } else {
+      videoMedia.appendChild(source);
       article.appendChild(videoMedia);
-
     }
-   
+
     article.appendChild(h2);
 
     return article;
