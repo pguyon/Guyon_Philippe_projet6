069d5ab541c155097966194df7079fac55ff6766
pguyon <pguyon69@gmail.com>
2022-05-22 14:13:20 +0200

    switch aria hidden on main when open or close modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,11 +1,13 @@
 function displayModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "block";
+  document.getElementById("main").setAttribute("aria-hidden", "true");
 }
 
 function closeModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "none";
+  document.getElementById("main").setAttribute("aria-hidden", "false");
 }
 
 const btnSubmit = document.getElementsByClassName("contact_button")[0];
