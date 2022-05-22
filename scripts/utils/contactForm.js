4aabd2cb9bf6980e1b7946575d53ec3e0d33c073
pguyon <pguyon69@gmail.com>
2022-05-22 13:36:47 +0200

    start form validation

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,30 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
 }
+
+const btnSubmit = document.getElementsByClassName("contact_button")[0];
+const firstname = document.getElementById("prenom");
+
+function checkFirstname() {
+  const firstname = document.getElementById("prenom").value.trim();
+  if (!firstname) {
+    console.log("pas de saisie");
+  } else if (firstname.length <= 4) {
+    console.log("saisie trop courte");
+  } else {
+    console.log("good");
+  }
+}
+
+function validate(e) {
+  e.preventDefault();
+  checkFirstname();
+}
+
+firstname.addEventListener("keyup", checkFirstname);
